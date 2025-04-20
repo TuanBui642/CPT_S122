1565b654fa28ab3a83345745104fe923943d968e
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:01:57 -0700

    Update Game.cpp

--- PA9_Game/PA9_Game/Game.cpp
+++ PA9_Game/PA9_Game/Game.cpp
@@ -1,110 +1,60 @@
-#pragma once
-#include "Game.hpp"
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*	File Programmer: Tyler Simmons
+*/
 
-Game::Game()
-{
-	this->initWindow();
-	this->initKeys();
-	this->initStates();
-}
+#ifndef GAME
+#define GAME
 
-Game::~Game()
-{
-	delete mWindow;
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+#include "Portal.hpp"
+#include "Platform.hpp"
+#include "Enemy.hpp"
+#include "GameState.hpp"
+#include "State.hpp"
+using std::cout;
+using std::endl;
 
-	while (!mStates.empty()) {
-		delete mStates.top();	//remove data
-		mStates.pop();			//remove pointer
-	}
-}
+class Game {
+public:
+    Game();
+    virtual ~Game();
 
-void Game::endApp()
-{
-	cout << "Ending game." << endl;
-}
+    //member functions
 
-void Game::update()
-{
-	this->updateSFMLEvents();
+    void endApp();
+    void update();
+    void updateSFMLEvents();
+    void updateDeltaTime();
 
-	if (!mStates.empty()) {
-		mStates.top()->update(mDeltaTime);
+    void render();
 
-		if (mStates.top()->getQuit()) {
-			mStates.top()->endState();
-			delete mStates.top();
-			mStates.pop();
-		}
-	}
+    void run();
 
-	//End game
-	else {
-		this->endApp();
-		mWindow->close();
-	}
-}
+    void drawMenu();
 
-void Game::updateSFMLEvents()
-{
-	while (const std::optional event = mWindow->pollEvent())
-	{
-		if (event->is<sf::Event::Closed>())
-			mWindow->close();
-	}
-}
+private:
+    sf::RenderWindow* mWindow;
+    float mDeltaTime;
+    sf::Clock mDTClock;
+    std::stack<State*> mStates;	//stack of states, keeps track of different types of states game gets in
+    //std::map<std::string, int> mSupportedKeys;	//an array-like structure of supported keys
 
-void Game::updateDeltaTime()
-{
-	mDeltaTime = mDTClock.restart().asSeconds();
-}
+    //private member functions
+    //initialization
+    void initWindow();
+    void initKeys();
+    void initStates();
+};
 
-void Game::render()
-{
-	mWindow->clear();
-
-	//render objects
-	if (!mStates.empty()) {
-		mStates.top()->render(mWindow);
-	}
-
-	mWindow->display();
-}
-
-void Game::run()
-{
-
-	while (mWindow->isOpen()) {
-		
-		this->updateDeltaTime();
-		this->update();
-		this->render();
-	}
-}
-
-void Game::drawMenu()
-{
-	//need to put stuff here later
-}
-
-void Game::initWindow()
-{
-	unsigned int width = 1280;
-	unsigned int height = 720;
-
-	mWindow = new sf::RenderWindow(sf::VideoMode({ width, height }), "Untitled 2D Platformer");
-
-	mWindow->setFramerateLimit(60);	//set frame rate cap to 60 fps
-}
-
-void Game::initKeys()
-{
-	/*mSupportedKeys.emplace("Escape", sf::Keyboard::Key::Escape);
-	mSupportedKeys.emplace("A", sf::Keyboard::Key::A);
-	mSupportedKeys.emplace("D", sf::Keyboard::Key::D);*/
-	//mSupportedKeys.emplace(" ", sf::Keyboard::Key::Space);
-}
-
-void Game::initStates()
-{
-	mStates.push(new GameState(mWindow));
-}
+#endif
