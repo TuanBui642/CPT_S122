a6a4caa2cb777675a14c4dd358925a1d5f7d3216
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:06:14 -0700

    Create HitboxComponent.hpp

---
+++ PA9_Game/PA9_Game/HitboxComponent.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include <stack>
+#include <map>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+
+class HitboxComponent {
+public:
+    HitboxComponent(sf::Sprite& sprite, float offsetX, float offsetY, float width,
+        float height);
+    virtual ~HitboxComponent();
+
+    //member functions
+    void update();
+    void render(sf::RenderTarget& target);
+    bool checkIntersect(const sf::FloatRect& rect);
+
+private:
+    sf::RectangleShape mHitbox;
+    sf::Sprite& mSprite;
+    float mOffsetX,
+        mOffsetY;
+};
