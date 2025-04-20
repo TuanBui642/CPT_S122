bd2417e2368fa27d9ce9a22347ee201bd47b90d3
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:09:34 -0700

    Create Test.hpp

---
+++ PA9_Game/PA9_Game/Test.hpp
@@ -0,0 +1,40 @@
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*/
+
+#ifndef TEST
+#define TEST
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stack>
+
+#include <SFML/Graphics.hpp>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::cin;
+using std::ostream;
+using std::ifstream;
+using std::ofstream;
+using std::vector;
+using std::stack;
+
+//File Includes Below:
+
+
+//Test Functions Declaration Below:
+void TestEnemyMovement();
+
+
+#endif
