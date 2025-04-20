1a6af04b7e6eb3c6715bae07e8b228149d0a1223
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:03:02 -0700

    Update GameState.hpp

--- PA9_Game/PA9_Game/GameState.hpp
+++ PA9_Game/PA9_Game/GameState.hpp
@@ -1,27 +1,63 @@
-#pragma once
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*	File Programmer: Tyler Simmons
+*/
+
+#ifndef GAMESTATE
+#define GAMESTATE
+
 #include <iostream>
 using std::cout;
 using std::endl;
 #include "State.hpp"	//this is a derived class of State
+#include "Enemy.hpp"
 
 class GameState : public State {
-	public:
-		GameState(sf::RenderWindow* window);
-		virtual ~GameState();
+public:
+    GameState(sf::RenderWindow* window);
+    virtual ~GameState();
 
-		//member functions
-		void updateInput(const float& deltaTime);
-		void update(const float& deltaTime);
-		void render(sf::RenderTarget* target = nullptr);
+    //member functions
+    void updateInput(const float& deltaTime);
+    void update(const float& deltaTime);
+    void render(sf::RenderTarget* target = nullptr);
 
-	private:
-		Player* mPlayer;
-		gameWorld worldLayout;
 
-		//member functions
-		void initKeyBindings();
-		void initTextures();
-		void initPlayers();
-		void initWorld();
+private:
+    //Initalization for Game (Possible Entities Present in Game)
+    Player* mPlayer;
+    bool mJumpUsed;
+
+    Enemy* mEnemy1;
+    Enemy* mEnemy2;
+    Enemy* mEnemy3;
+    Enemy* mEnemy4;
+    Enemy* mEnemy5;
+
+    gameWorld worldLayout; 
+
+    //Time (real world seconds) usage
+    sf::Clock OngoingTime;
+    sf::Time Time;
+
+
+    //member functions
+    bool checkJump();
+    void initKeyBindings();
+    void initTextures();
+    void initPlayers();
+    void initEnemies();
+  
+    //Ashton Work: initiate world's object and collision between player & object
+    void initWorld();
 
 };
+
+#endif
