dc16842a7d1748df59cb3dbd4358a454d4f38f26
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:04:15 -0700

    Update Player.hpp

--- PA9_Game/PA9_Game/Player.hpp
+++ PA9_Game/PA9_Game/Player.hpp
@@ -1,12 +1,39 @@
-#pragma once
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*	File Programmer: Tyler Simmons
+*/
+
+#ifndef PLAYER
+#define PLAYER
+
+
 #include "Entity.hpp"
 
 class Player : public Entity {
-	public:
-		Player(float x_pos, float y_pos, sf::Texture* texture);
-		virtual ~Player();
+public:
+	Player(float x_pos, float y_pos, sf::Texture& textureSheet);
+	virtual ~Player();
 
-	private:
-		void initVariables();
-		void initComponents();
+	//getters and setters
+	bool getJumpUsed() const;
+	//member functions
+	virtual void update(const float& deltaTime);
+
+
+private:
+	bool mJumpUsed;
+	bool mIsFalling;
+
+	//member functions
+	void initVariables();
+	void initComponents();
 };
+
+#endif
