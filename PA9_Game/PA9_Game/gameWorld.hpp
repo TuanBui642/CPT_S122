1f8d082cf62d5be7cb8705858655acc0ec129c58
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:05:08 -0700

    Update gameWorld.hpp

--- PA9_Game/PA9_Game/gameWorld.hpp
+++ PA9_Game/PA9_Game/gameWorld.hpp
@@ -1,82 +1,41 @@
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*	File Programmer: Ashton Palma
+*/
+
 #ifndef GAMEWORLD_H
 #define GAMEWORLD_H
 
-#include "gameTile.hpp"
+#include <vector>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class gameTile; //forward-declare
 
 class gameWorld
 {
 private:
-	std::vector<gameTile*> tiles; //converts tiles to a vector type (shortcut)
+	std::vector <gameTile*> tiles; //converts tiles to a vector type (shortcut)
 	int windowWidth;
 	int windowHeight;
 
 public:
-	gameWorld(const int width, const int height)
-	{
-		this->windowHeight = height;
-		this->windowWidth = width;
-	}
+	gameWorld(const int width, const int height);
+	~gameWorld();
 
-	~gameWorld()
-	{
-		for (int i = 0; i < tiles.size(); i++)
-		{
-			delete tiles[i];
-		}
-	}
 
-	//Functions that generate different objects
-	//parameter( start coord (x,y), end coord (x,y), tile size (x,y)
-	void generatePlatform(sf::Vector2f posStr, sf::Vector2f posEnd, sf::Vector2f tileSize)
-	{
-		for (float i = posStr.x; i <= posEnd.x; i += tileSize.x)
-		{	//insert a new tile after the previous
+	void generateBlock(sf::Vector2f posStr, sf::Vector2f posEnd, sf::Vector2f tileSize, sf::Texture& texture);
 
-			sf::Vector2f newPos = { i, posStr.y };
-			tiles.push_back(new gameTile("platform", tileSize, newPos));
-		}
-	}
+	void checkCollisions(sf::RectangleShape& person, float& velocityY, bool& isJumping);
 
-	void generateWall(sf::Vector2f posStr, sf::Vector2f posEnd, sf::Vector2f tileSize)
-	{
-		for (float i = posStr.y; i <= posEnd.y; i += tileSize.y)
-		{	//insert a new tile after the previous
-
-			sf::Vector2f newPos = { posStr.x, i };
-			tiles.push_back(new gameTile("wall", tileSize, newPos));
-		}
-	}
-
-	void generateBlock(sf::Vector2f posStr, sf::Vector2f posEnd, sf::Vector2f tileSize)
-	{
-		for (float i = posStr.y; i <= posEnd.y; i += tileSize.y)
-		{
-			for (float k = posStr.x; k <= posEnd.x; k += tileSize.x)
-			{
-				sf::Vector2f newPos = { k, i };
-				tiles.push_back(new gameTile("Block", tileSize, newPos));
-			}
-		}
-	}
-
-	void checkCollisions(sf::RectangleShape& person, float& velocityY, bool& isJumping)
-	{
-		for (int i = 0; i < tiles.size(); i++)
-		{
-			if (tiles[i])
-			{
-				tiles[i]->collisionStatus(person, velocityY, isJumping, 0);
-			}
-		}
-	}
-
-	void draw(sf::RenderWindow& window)
-	{
-		for (int i = 0; i < tiles.size(); ++i)
-		{
-			tiles[i]->render(&window);
-		}
-	}
+	void draw(sf::RenderWindow& window);
 
 
 };
