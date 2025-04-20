a1417851037d68e7d16414b6d21744e547933560
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:07:12 -0700

    Create AnimationComponent.cpp

---
+++ PA9_Game/PA9_Game/AnimationComponent.cpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "AnimationComponent.hpp"
+
+AnimationComponent::AnimationComponent(sf::Sprite& sprite, sf::Texture& textureSheet)
+	: mSprite(sprite), mTextureSheet(textureSheet), mpLastAnimation(nullptr)
+{
+
+}
+
+AnimationComponent::~AnimationComponent()
+{
+	for (auto& i : mAnimations) {
+		delete i.second;
+	}
+}
+
+void AnimationComponent::play(const std::string key, const float& deltaTime)
+{
+	//reset previous animation
+	if (mpLastAnimation != mAnimations[key]) {
+		if (mpLastAnimation == nullptr) {
+			mpLastAnimation = mAnimations[key];
+		}
+		else {
+			mpLastAnimation->reset();
+			mpLastAnimation = mAnimations[key];
+		}
+	}
+
+	//play animation
+	mAnimations[key]->play(deltaTime);
+}
+
+void AnimationComponent::addAnimation(const std::string key, int startFrameX, int startFrameY, int frames_x, int frames_y,
+	int width, int height, float animationTimer)
+{
+	mAnimations[key] = new Animation(this->mSprite, this->mTextureSheet, startFrameX, startFrameY, frames_x,
+		frames_y, width, height, animationTimer);
+}
