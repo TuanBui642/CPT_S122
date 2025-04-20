b53a0da9b61b822c88eb6eccfd17b0e8469bf30f
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:05:32 -0700

    Create AnimationComponent.hpp

---
+++ PA9_Game/PA9_Game/AnimationComponent.hpp
@@ -0,0 +1,36 @@
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*	File Programmer: Tyler Simmons
+*/
+
+#ifndef ANIMATIONCOMPONENT
+#define ANIMATIONCOMPONENT
+
+#include "Animation.hpp"
+
+class AnimationComponent {
+public:
+	//constructor & destructor
+	AnimationComponent(sf::Sprite& sprite, sf::Texture& textureSheet);
+	virtual ~AnimationComponent();
+
+	//member functions
+	void play(const std::string key, const float& deltaTime);
+	void addAnimation(const std::string key, int startFrameX, int startFrameY, int frames_x,
+		int frames_y, int width, int height, float animationTimer);
+
+private:
+	std::map<std::string, Animation*> mAnimations;
+	sf::Sprite& mSprite;
+	sf::Texture& mTextureSheet;
+	Animation* mpLastAnimation;
+};
+
+#endif
