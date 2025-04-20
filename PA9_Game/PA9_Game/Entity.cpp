24c9bc959381259beca699b4ead0f59c0c77ce10
Tuan_Bui <tuan.q.bui@wsu.edu>
2025-04-20 00:01:31 -0700

    Update Entity.cpp

--- PA9_Game/PA9_Game/Entity.cpp
+++ PA9_Game/PA9_Game/Entity.cpp
@@ -1,50 +1,85 @@
+/*
+*	Program: PA9 (Group Game Project)
+*	Date: 4/12/25
+*	Group:
+*		Tuan Bui
+*		Ashton Palma
+*		Tyler Simmons
+*		Brandon Weirth
+*
+*	File Programmer: Tyler Simmons
+*/
+
 #pragma once
+
 #include "Entity.hpp"
 
-Entity::Entity()
+
+Entity::Entity(sf::Sprite sprite, sf::Texture texture) : mpSprite(texture)
 {
 	this->initVariables();
 }
 
 Entity::~Entity()
 {
-	delete mpSprite;
+	delete mpHitboxComponent;
+	delete mpMovementComponent;
+	delete mpAnimationComponent;
 }
 
 void Entity::setPosition(const float x_pos, const float y_pos)
 {
-	if (mpSprite != nullptr) {
-		mpSprite->setPosition(sf::Vector2f(x_pos, y_pos));
-	}
+	mpSprite.setPosition(sf::Vector2f(x_pos, y_pos));
 }
 
-void Entity::createSprite(sf::Texture* texture)
+void Entity::setTexture(sf::Texture& texture)
 {
-	mpTexture = texture;
-	mpSprite = new sf::Sprite(*mpTexture);
+	mpSprite.setTexture(texture);
+}
+
+void Entity::createHitboxComponent(sf::Sprite& sprite, float offsetX, float offsetY,
+	float width, float height)
+{
+	mpHitboxComponent = new HitboxComponent(sprite, offsetX, offsetY, width, height);
+}
+
+void Entity::createMovementComponent(const float maxVelocity, const float acceleration,
+	const float deceleration)
+{
+	mpMovementComponent = new MovementComponent(mpSprite, acceleration,deceleration);
+}
+
+void Entity::createAnimationComponent(sf::Texture& textureSheet)
+{
+	mpAnimationComponent = new AnimationComponent(mpSprite, textureSheet);
 }
 
 void Entity::move(const float deltaTime, const float dir_x, const float dir_y)
 {
-	if (mpSprite != nullptr) {
-		mpSprite->move({ dir_x * mVelocity * deltaTime, dir_y * mVelocity * deltaTime });
+	if (mpMovementComponent != nullptr) {
+		mpMovementComponent->move(dir_x, dir_y, deltaTime);
 	}
 }
 
 void Entity::update(const float& deltaTime)
 {
+	if (mpMovementComponent != nullptr) {
+		mpMovementComponent->update(deltaTime);
+	}
 }
 
-void Entity::render(sf::RenderTarget* target)
+void Entity::render(sf::RenderTarget& target)
 {
-	if (mpSprite != nullptr) {
-		target->draw(*mpSprite);
+	target.draw(mpSprite);
+
+	if (mpHitboxComponent) {
+		mpHitboxComponent->render(target);
 	}
 }
 
 void Entity::initVariables()
 {
-	mpTexture = nullptr;
-	mpSprite = nullptr;
-	mVelocity = 100.0f;
+	mpHitboxComponent = nullptr;
+	mpMovementComponent = nullptr;
+	mpAnimationComponent = nullptr;
 }
